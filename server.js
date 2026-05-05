4a7454ec65317d8d5062347dc6ac774f783e33ff
JhonWick9191 <singhaditya5329@gmail.com>
2026-05-05 12:33:49 +0530

    adding code for removing cross origin error

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port
+    origin: 'https://cult.fitness', // Vite default port
     credentials: true
 }));
 
