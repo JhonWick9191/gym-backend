31730a50706c61641194a59c993a443896cf5920
JhonWick9191 <singhaditya5329@gmail.com>
2026-05-14 21:23:45 +0530

    adding code

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ const app = express();
 app.use(cors({
     origin: [
         'https://cult.fitness',
-        'https://www.cult.fitness'
+        'https://www.cult.fitness',
+        'http://localhost:5173/'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
